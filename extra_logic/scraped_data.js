41e46fce531d8e1a907228f973b8a7fb4f6ff22b
RSR312-206 <roccosummer@gmail.com>
2015-10-02 09:37:10 -0700

    adding extra logic for data scrape

--- extra_logic/scraped_data.js
+++ extra_logic/scraped_data.js
@@ -1,3 +1,7 @@
+//todo: build a function that handles the href tags and removes all invalid
+//or non-relevant urls
+//then attach the website domain to the links that begin with a '/' so the
+//full url can be evaluated with response.statusCode
 var exceptions =  {
 //trying to clean the html code that is scraped in
   check_scraped_data: function(href) {
@@ -7,6 +11,7 @@ var exceptions =  {
     }
     var a = href.split('');
     if (a[0] == '/' && a[0] !== a) {
+      //getting the domain here?
       a = domain + a;
       urls.push(a);
     }
